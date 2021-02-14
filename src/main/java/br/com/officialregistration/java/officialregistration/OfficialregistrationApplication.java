a68c8bc633fbab013a72d3526e3ab80bba6a49ea
Leonardo Fernandes <>
2021-02-14 14:01:56 -0300

    FIRST COMMIT

---
+++ src/main/java/br/com/officialregistration/java/officialregistration/OfficialregistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.officialregistration.java.officialregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/officialregistration/java/officialregistration/OfficialregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.officialregistration.java.officialregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
